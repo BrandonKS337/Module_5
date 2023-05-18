19f52317703fdf2c9a245ad3a7c52784bbfe3a3e
Project V2 <brandonks337@gmail.com>
2023-05-17 21:47:44 -0500

    adding last two modified files before pushing

---
+++ Server/controller/calculatorController.js
@@ -0,0 +1,20 @@
+const Calculator = require("../libraries/calculator.js")     //typed out 
+const myCalc = new Calculator()
+
+const addNumbers = (req, res) => {
+  console.log(req.query);
+  let num1 = parseInt(req.query.num1); //change our query number into an integer
+  let num2 = parseInt(req.query.num2);
+
+//   let sum = num1 + num2;  //REMOVED DURING CLASS TO DO LIBRARY STUFF
+let sum = myCalc.add(num1,num2)
+  console.log("Sum", sum); // res.send('Add')
+  res.status(200).json({ result: sum }); //included to show server result A-ok
+};
+const testFunction = () => {
+    myCalc.test()
+};
+
+module.exports = {
+  addNumbers,
+};
--- Server/routes/calculatorRoute.js
+++ Server/routes/calculatorRoute.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const router = express.Router(); //pulling out function from express package
 
-router.get("/add", (req, res) => {
-  /// 4444 NOTES by Robert: 4444 need a total of 4, ex changing /add to  /subtract so so forth
-  console.log(req.query);
-  let num1 = parseInt(req.query.num1); //change our query number into an integer
-  let num2 = parseInt(req.query.num2);
+const calculatorController = require('../controller/calculatorController')
 
-  let sum = num1 + num2;
-  console.log("Sum", sum);
-  // res.send('Add')
-  res.status(200).json({ result: sum }); //included to show server result A-ok
-});
+router.get('/add', (req, res) => {
+    calculatorController.addNumbers(req, res)
+})
 
-module.exports = router; // exports results as a module type... we are only exporting router in this example
+module.exports = router;
 
-/// 3333 NOTES by Robert: notes on routes 33333: have source just need to change it around to create new routes.
-/// NOTES by Robert:actually just need to copy and paste then change it... robert 5/17 7:26PM.. to correct operator
+
+// router.get("/add", (req, res) => {
+//   /// 4444 NOTES by Robert: 4444 need a total of 4, ex changing /add to  /subtract so so forth
+//   console.log(req.query);
+//   let num1 = parseInt(req.query.num1); //change our query number into an integer
+//   let num2 = parseInt(req.query.num2);
+
+//   let sum = num1 + num2;
+//   console.log("Sum", sum);   // res.send('Add')
+//   res.status(200).json({ result: sum }); //included to show server result A-ok
+// });
+
+// module.exports = router; // exports results as a module type... we are only exporting router in this example
+
+// /// 3333 NOTES by Robert: notes on routes 33333: have source just need to change it around to create new routes.
+// /// NOTES by Robert:actually just need to copy and paste then change it... robert 5/17 7:26PM.. to correct operator
